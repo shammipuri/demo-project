224a03db7b55675260a573053c9d583fd46b66a8
Shammi Puri <shammipuri123@gmail.com>
2013-06-11 15:44:43 +0530

    added home page

---
+++ index.js
@@ -0,0 +1,55 @@
+function show(element)
+{
+	if(element==1)
+	{	
+		if(document.getElementById("submenu1").style.visibility=="hidden")
+		{	
+			document.getElementById("submenu1").style.visibility="visible";
+			document.getElementById("submenu1").style.height="100%";
+		}
+		else
+		{
+			document.getElementById("submenu1").style.visibility="hidden";
+			document.getElementById("submenu1").style.height="0px";
+		}	
+	}	
+	else if(element==2)
+	{	
+		if(document.getElementById("submenu2").style.visibility=="hidden")
+		{	
+			document.getElementById("submenu2").style.visibility="visible";
+			document.getElementById("submenu2").style.height="100%";
+		}
+		else
+		{
+			document.getElementById("submenu2").style.visibility="hidden";
+			document.getElementById("submenu2").style.height="0px";
+		}	
+	}
+	else if(element==3)
+	{	
+		if(document.getElementById("submenu3").style.visibility=="hidden")
+		{	
+			document.getElementById("submenu3").style.visibility="visible";
+			document.getElementById("submenu3").style.height="100%";
+		}
+		else
+		{
+			document.getElementById("submenu3").style.visibility="hidden";
+			document.getElementById("submenu3").style.height="0px";
+		}	
+	}
+	else
+	{	
+		if(document.getElementById("submenu4").style.visibility=="hidden")
+		{	
+			document.getElementById("submenu4").style.visibility="visible";
+			document.getElementById("submenu4").style.height="100%";
+		}
+		else
+		{
+			document.getElementById("submenu4").style.visibility="hidden";
+			document.getElementById("submenu4").style.height="0px";
+		}	
+	}
+}
